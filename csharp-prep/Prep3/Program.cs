2504762d7ec0378323587d3f730d2d1a0b905b5d
BrycePetrucka <pet20101@byui.edu>
2025-01-17 12:40:46 -0700

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magic_number;
+        Random randomGenerator = new Random();
+        magic_number = randomGenerator.Next(1,101);
+        int guess = -1;
+
+        while (magic_number != guess)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magic_number)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (guess > magic_number)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("Nice, you guessed it!");
+            }
+        }
+
+        
+        
     }
 }
