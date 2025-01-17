fc89262cd0648d95f9a624c12d40aaad6cf355e7
BrycePetrucka <pet20101@byui.edu>
2025-01-17 12:11:17 -0700

    changes made in class

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Xml.XPath;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        /*
+        this is how you do mulitple line comments
+        */
+
+        //Adj stands for adjective
+        string adj = GetAdjective();
+        string noun = GetNoun();
+        int number = Multiply(17, 44);
+        Console.WriteLine($"I looked out the window and saw {number} {adj} {noun}.");
+    }
+
+    static int Multiply(int number1, int number2)
+    {
+        int result = number1 * number2;
+        return result;
+    }
+
+    static string GetAdjective()
+    {
+        List<string> words = new List<string>();
+        words.Add("blue");
+        words.Add("green");
+        words.Add("small");
+        string adj = words[2];
+        return adj;
+    }
+
+    static string GetNoun()
+    {
+        string word = "bird";
+        return word;
     }
 }
